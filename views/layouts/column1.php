19387d50d6dbdb0aa29e387e596ddefc2492f6b5
Philipp Frenzel <philipp@frenzel.net>
2014-05-23 15:51:16 +0200

    fix

--- views/layouts/column1.php
+++ views/layouts/column1.php
@@ -1,4 +1,4 @@
-<?php $this->beginContent('@app/views/layouts/'.\frenzelgmbh\cmaddress\Module::getMainLayout().'.php'); ?>
+<?php $this->beginContent('@app/views/layouts/main.php'); ?>
 <div id="content">
   <div class="cm-address">
     <?= $content; ?>
--- views/layouts/column2.php
+++ views/layouts/column2.php
@@ -1,4 +1,4 @@
-<?php $this->beginContent('@app/views/layouts/'.\frenzelgmbh\cmaddress\Module::getMainLayout().'.php'); ?>
+<?php $this->beginContent('@app/views/layouts/main.php'); ?>
 <div id="content">
   <div class="row">
     <div class="col-md-4">      
