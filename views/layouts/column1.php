048381b947eb963b0ce2d3f67ab5ce23329425ab
Philipp Frenzel <philipp@frenzel.net>
2015-05-26 21:02:12 +0200

    removing not needed layouts

--- views/layouts/column1.php
+++
@@ -1,7 +0,0 @@
-<?php $this->beginContent('@app/views/layouts/main.php'); ?>
-<div id="content">
-  <div class="cm-address">
-    <?= $content; ?>
-  </div>
-</div><!-- container -->
-<?php $this->endContent(); ?>
