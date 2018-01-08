fcea81cd08b149aaaba34b298819b4b2ac786afc
Philipp Frenzel <pf@com-x-cha.com>
2018-01-08 21:04:32 +0100

    Update m180108_200000_ext_addresstables.php

--- migrations/m180108_200000_ext_addresstables.php
+++ migrations/m180108_200000_ext_addresstables.php
@@ -1,3 +1,24 @@
-## A New Post
+<?php
 
-Enter text in [Markdown](http://daringfireball.net/projects/markdown/). Use the toolbar above, or click the **?** button for formatting help.
+/**
+ * The migration script for the addresses subdivision
+ * @author Philipp Frenzel <philipp@frenzel.net>
+ * @copyright Frenzel GmbH
+ * @version 1.1
+ */
+
+use yii\db\Schema;
+
+class m180108_200000_ext_addresstables extends \yii\db\Migration
+{
+	public function up()
+	{ 
+    	$this->addColumn('{{%net_frenzel_country}}', 'is_active', Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 1');
+    }
+  
+    public function down()
+    {
+      $this->dropColumn('{{%net_frenzel_country}}', 'is_active');
+      return true;
+    }
+}
